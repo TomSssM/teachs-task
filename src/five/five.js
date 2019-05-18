289458a9bda59f82af6a13e3c16725aecdc368ff
TomSssM <ilyashome3@gmail.com>
2019-05-19 02:54:02 +0300

    fix: mixed request bug

--- src/five/five.js
+++ src/five/five.js
@@ -5,7 +5,7 @@ const usdInput = document.querySelector('#js-usd-input');
 const output = document.querySelector('#js-output');
 
 const key = '5888f321e45149c7189237658ed2d0ec';
-const url = `http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,BYN&format=1`;
+const url = `https://data.fixer.io/api/latest?access_key=${key}&symbols=USD,BYN&format=1`;
 
 document.querySelector('#js-calc-btn').addEventListener('click', async () => {
     const res = await fetch(url);
@@ -15,4 +15,4 @@ document.querySelector('#js-calc-btn').addEventListener('click', async () => {
     output.innerHTML = result;
 });
 
-// { "success": true, "timestamp": 1558207025, "base": "EUR", "date": "2019-05-18", "rates": { "USD": 1.116744, "BYN": 2.327016 } }
+
